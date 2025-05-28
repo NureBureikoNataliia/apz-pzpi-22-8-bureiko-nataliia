055b0915682022e2322a6e52da629abaa2885a1e
Nataliia Bureiko <nataliia.bureiko@nure.ua>
2025-05-28 21:12:27 +0200

    Delete Lab2/pzpi-22-8-bureiko-nataliia-lab2/backend directory

--- Lab2/pzpi-22-8-bureiko-nataliia-lab2/backend/showcaseProduct.js
+++
@@ -1,8 +0,0 @@
-let showcaseProductId = null;
-
-const getShowcaseProduct = () => showcaseProductId;
-const setShowcaseProduct = (id) => {
-  showcaseProductId = id;
-};
-
-module.exports = { getShowcaseProduct, setShowcaseProduct };
